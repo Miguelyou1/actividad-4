8c5a1bd44de6a5e621edfb739bcf528f30885c6f
Miguelyou1 <123724242+Miguelyou1@users.noreply.github.com>
2023-04-14 09:18:02 +0200

    script

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 let primerNumero
 alert('Hola, a continuación se le pedirá un número para realizar unas operaciones ');
 primerNumero = prompt("introduzca un numero");
+let segundoNumero;
+segundoNumero = prompt('introduzca un numero')
